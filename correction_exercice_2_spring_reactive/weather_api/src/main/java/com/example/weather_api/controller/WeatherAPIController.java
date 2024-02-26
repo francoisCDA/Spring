85f7e1e18891dc80d759cab67424de659779a150
François Termote <francois.termote@gmail.com>
2024-02-26 17:31:03 +0100

    exo reactif

---
+++ correction_exercice_2_spring_reactive/weather_api/src/main/java/com/example/weather_api/controller/WeatherAPIController.java
@@ -0,0 +1,27 @@
+package com.example.weather_api.controller;
+
+
+import com.example.weather_api.entity.WeatherForecast;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Random;
+
+@RequestMapping("weather")
+@RestController
+public class WeatherAPIController {
+
+    private final Random random;
+
+    public WeatherAPIController() {
+        random = new Random();
+    }
+
+    @GetMapping("{city}")
+    public Flux<WeatherForecast> get(String city) {
+        return Flux.just(WeatherForecast.builder().city(city).temperature(random.nextDouble(20)).build(),WeatherForecast.builder().city(city).temperature(random.nextDouble(20)).build());
+    }
+}
