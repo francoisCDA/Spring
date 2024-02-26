64ca90ae48c768380f8c73d87100aed1edfe1300
François Termote <francois.termote@gmail.com>
2024-02-26 15:20:12 +0100

    exo reactif agregation avec 2 services

--- exo_WebFlux_Agregation/src/main/java/org/example/exo_webflux_agregation/controller/NewsAPI.java
+++ exo_WebFlux_Agregation/src/main/java/org/example/exo_webflux_agregation/controller/NewsAPI.java
@@ -3,9 +3,8 @@ package org.example.exo_webflux_agregation.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.exo_webflux_agregation.service.NewsService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
@@ -26,7 +25,14 @@ public class NewsAPI {
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> getWeather() {
 
-        return Flux.just("grève ", "manifestation ", "charge de CRS ").delayElements(Duration.ofSeconds(2));
+        return newsService.getFlux();
     }
 
+    @PostMapping
+    public ResponseEntity<String> postWeather(@RequestParam String news) {
+
+        newsService.sendNews(news);
+        return ResponseEntity.ok("info buletin météo " + news);
+
+    }
 }
--- exo_WebFlux_Agregation/src/main/java/org/example/exo_webflux_agregation/controller/WeatherAPI.java
+++ exo_WebFlux_Agregation/src/main/java/org/example/exo_webflux_agregation/controller/WeatherAPI.java
@@ -1,16 +1,20 @@
 package org.example.exo_webflux_agregation.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.exo_webflux_agregation.service.WeatherService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/weather")
+@RequiredArgsConstructor
 public class WeatherAPI {
+
+    private final WeatherService weatherService;
 
     @GetMapping(value = "flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> getWeatherFlux() {
@@ -21,7 +25,16 @@ public class WeatherAPI {
     @GetMapping( MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> getWeather() {
 
-        return Flux.just("pluie ", "soleil ", "pluie ", "neige").delayElements(Duration.ofSeconds(1));
+        return weatherService.getFlux();
     }
+
+    @PostMapping
+    public ResponseEntity<String> postWeather(@RequestParam String weather) {
+
+        weatherService.update(weather);
+        return ResponseEntity.ok("info buletin météo " + weather);
+
+    }
+
 
 }
