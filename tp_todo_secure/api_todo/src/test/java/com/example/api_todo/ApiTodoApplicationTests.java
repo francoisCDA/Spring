d765cf46416038dab3c8b9cccb168111cb4088e8
François Termote <francois.termote@labom2iformation.fr>
2024-03-25 08:35:10 +0100

    update

---
+++ tp_todo_secure/api_todo/src/test/java/com/example/api_todo/ApiTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
